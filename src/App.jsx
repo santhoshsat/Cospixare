f6b23d17ee378de3d7078e9d2beca1a5e2e9a02f
Jai-jayapalan <jaijayathilak@gmail.com>
2025-05-02 17:32:56 +0530

    added utilities, home,about and navbar components and then data folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import { GoogleAnalytics } from './components/GoogleAnalytics'
+import { CustomCursor, CustomPreLoader } from './utils'
+import { NavBar, Home, About } from './components'
 
 function App() {
 
   return (
     <>
       <h1>CosPixare Technology</h1>
+      <CustomPreLoader />
+      <CustomCursor />
+      <GoogleAnalytics />
+      <NavBar />
+      <Home />
+      <About />
     </>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as NavBar } from './NavBar/Navbar'
+export { default as About } from './About/About'
+export { default as Home } from './Home/Home'
